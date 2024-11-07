26db7135005b9bb1665e43fa4c810f74c6ca2406
greysonacker <greysonacker@tamu.edu>
2024-11-07 17:35:12 -0600

    Update attitude_calculations_INWORK.py
    Corrected units of S and C

--- Aerodynamics/attitude_calculations_INWORK.py
+++ Aerodynamics/attitude_calculations_INWORK.py
@@ -8,9 +8,9 @@ def attitude_angle(chord_length, coeff_lift, prop_diameter):
     the propellor's thrust vector is pointing straight up. Positive pitch indicates a forward pitch of the nose, while a
     negative pitch indicates a backward nose pitch.
 
-    :param chord_length: Chord length of wing in meters
+    :param chord_length: Chord length of wing in feet
     :param coeff_lift: Nondimensional infinite coefficient of lift for one wing
-    :param prop_diameter: Propellor diameter in meters
+    :param prop_diameter: Propellor diameter in feet
     :returns: Pitch angle in degrees
     """
     pitch = np.arctan((4 * chord_length * coeff_lift) / (np.pi * prop_diameter))
